b9d44b752cc2c8e0353e5a9617d64120f26edb7f
Roland Hauser <sourcepond@gmail.com>
2017-10-05 15:01:03 +0200

    Do only include local resources

--- spring-web-blueprint/src/main/java/ch/sourcepond/spring/web/blueprint/internal/ClasspathResolver.java
+++ spring-web-blueprint/src/main/java/ch/sourcepond/spring/web/blueprint/internal/ClasspathResolver.java
@@ -22,6 +22,7 @@ import org.springframework.util.PathMatcher;
 import java.net.URL;
 import java.util.Collection;
 
+import static org.osgi.framework.wiring.BundleWiring.LISTRESOURCES_LOCAL;
 import static org.osgi.framework.wiring.BundleWiring.LISTRESOURCES_RECURSE;
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -50,7 +51,7 @@ class ClasspathResolver extends InternalResolver<String> {
     protected Collection<String> listAllResources(final Bundle bundle) {
         final BundleWiring wiring = bundleWiring(bundle);
         LOG.debug("Bundle state of {} is {}, wiring is in use: {}", bundle.getSymbolicName(), bundle.getState(), wiring.isInUse());
-        return bundleWiring(bundle).listResources("/", "*", LISTRESOURCES_RECURSE);
+        return bundleWiring(bundle).listResources("/", "*", LISTRESOURCES_LOCAL & LISTRESOURCES_RECURSE);
     }
 
     /*
