e657f6d4f7ba01c299c049ef97cd5646dcaef463
Roland Hauser <sourcepond@gmail.com>
2017-10-05 15:52:29 +0200

    Reverted change

--- spring-web-blueprint/src/main/java/ch/sourcepond/spring/web/blueprint/internal/ClasspathResolver.java
+++ spring-web-blueprint/src/main/java/ch/sourcepond/spring/web/blueprint/internal/ClasspathResolver.java
@@ -51,7 +51,7 @@ class ClasspathResolver extends InternalResolver<String> {
     protected Collection<String> listAllResources(final Bundle bundle) {
         final BundleWiring wiring = bundleWiring(bundle);
         LOG.debug("Bundle state of {} is {}, wiring is in use: {}", bundle.getSymbolicName(), bundle.getState(), wiring.isInUse());
-        return bundleWiring(bundle).listResources("/", "*", LISTRESOURCES_LOCAL & LISTRESOURCES_RECURSE);
+        return bundleWiring(bundle).listResources("/", "*", LISTRESOURCES_RECURSE);
     }
 
     /*
